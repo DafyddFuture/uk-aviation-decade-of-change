0d9d06bf3f06b0797f6bd63257b7e41515e44e74
Dafydd Jones <dafyddfuture@gmail.com>
2026-09-13 00:24:33 +0100

    Automate monthly CAA Table 09 downloads

--- src/download_table09.py
+++ src/download_table09.py
@@ -1,27 +1,33 @@
+import calendar
 import requests
-
-url = "https://www.caa.co.uk/data-and-analysis/uk-aviation-market/airports/uk-airport-data/uk-airport-data-2015/january-2015/"
-
-response = requests.get(url)
-
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-soup = BeautifulSoup(response.text, "html.parser")
-links = soup.find_all("a")
 
-for link in links:
-    if "Table 09" in link.get_text() and "CSV" in link.get_text():
-        print(link.get_text(strip=True))
-        csv_url = urljoin(url, link.get("href"))
-        print(csv_url)
-        csv_response = requests.get(csv_url)
-        print(csv_response.status_code)
+year = 2015
+for month in range(1, 13):
+    month_name = calendar.month_name[month].lower()
 
-        file_path = "data/raw/table09_2015_01.csv"
+    url = f"https://www.caa.co.uk/data-and-analysis/uk-aviation-market/airports/uk-airport-data/uk-airport-data-{year}/{month_name}-{year}/"
 
-with open(file_path, "wb") as file:
-    file.write(csv_response.content)
+    response = requests.get(url)
 
-print(response.status_code)
-print(len(links))
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all("a")
+
+    for link in links:
+        if "Table 09" in link.get_text() and "CSV" in link.get_text():
+            print(link.get_text(strip=True))
+            csv_url = urljoin(url, link.get("href"))
+            print(csv_url)
+            csv_response = requests.get(csv_url)
+            print(csv_response.status_code)
+
+            file_path = f"data/raw/table09_{year}_{month:02d}.csv"
+
+            with open(file_path, "wb") as file:
+                file.write(csv_response.content)
+
+            print(response.status_code)
+            print(len(links))
